b122318c08596fff533c5dd3d1f0f0915f1b7871
GUEBLAOUI Samir <samir.gueblaoui@hotmail.com>
2026-04-22 19:33:37 +0200

    Resolution de log_levels

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -2,14 +2,23 @@
 
 namespace log_line {
 std::string message(std::string line) {
-    // return the message
+    if(line[1] == 'E') return line.substr(9); 
+    if (line[1] == 'I') return line.substr(8); 
+    if (line[1] == 'W') return line.substr(11); 
+    return "Mauvais format";
 }
 
 std::string log_level(std::string line) {
-    // return the log level
+    if(line[1] == 'E') return "ERROR"; 
+    if (line[1] == 'I') return "INFO"; 
+    if (line[1] == 'W') return "WARNING"; 
+    return "Mauvais format";
 }
 
 std::string reformat(std::string line) {
-    // return the reformatted message
+    if(line[1] == 'E') return line.substr(9) + " (ERROR)"; 
+    if (line[1] == 'I') return line.substr(8) + " (INFO)"; 
+    if (line[1] == 'W') return line.substr(11) + " (WARNING)"; 
+    return "Mauvais format";
 }
 }  // namespace log_line
