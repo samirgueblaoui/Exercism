008a7edf51c4d22e58e27c46959b9ff0be1037a0
GUEBLAOUI Samir <samir.gueblaoui@hotmail.com>
2026-04-23 16:40:50 +0200

    Resolution de leap

---
+++ cpp/reverse-string/reverse_string.cpp
@@ -0,0 +1,7 @@
+#include "reverse_string.h"
+
+namespace reverse_string {
+
+// TODO: add your solution here
+
+}  // namespace reverse_string
---
+++ cpp/reverse-string/reverse_string.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace reverse_string {
+
+// TODO: add your solution here
+
+}  // namespace reverse_string
